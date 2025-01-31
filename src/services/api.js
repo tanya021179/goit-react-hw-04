05f7c72b07af1d2d240aceaca632160c6dceae42
Tetiana Prokopenko <115223555+tanya021179@users.noreply.github.com>
2025-01-31 16:56:11 +0100

    change style

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,7 @@ const keyUnsplash = "O-LOnRWHqyvxv1velTIsZ80G62iIX12mrAE3fPe_Osw";
 
 export const fetchArticles = async (query, page) => {
   const response = await axios.get(
-    `https://api.unsplash.com/search/photos?query=${query}&page=${page}&client_id=${keyUnsplash}`,
-    { credentials: "omit" }
+    `https://api.unsplash.com/search/photos?query=${query}&page=${page}&client_id=${keyUnsplash}`
   );
   return response.data.results;
 };
